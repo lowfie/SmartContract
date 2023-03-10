7624f18fdf248d6a74075c0a69fa119fc127536b
Andrew <vesell4123@mail.ru>
2023-03-10 19:46:36 +0300

    set database settings

--- app/settings/config.py
+++ app/settings/config.py
@@ -1,3 +1,16 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+# app settings
 UVICORN_RELOAD = True
 API_NAME = "SmartContract"
 API_VERSION = "0.1"
+
+# database configuration
+USER_POSTGRES = env.str("USER_POSTGRES")
+PASSWORD_POSTGRES = env.str("PASSWORD_POSTGRES")
+HOST_POSTGRES = env.str("HOST_POSTGRES")
+PORT_POSTGRES = env.str("PORT_POSTGRES")
+DATABASE_POSTGRES = env.str("DATABASE_POSTGRES")
