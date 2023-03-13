b6f1a93b7fcc897e655392223de709fd34088a1c
Andrew <vesell4123@mail.ru>
2023-03-14 01:57:54 +0300

    change str to variable from config

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import uvicorn
 from tortoise import run_async
 
-from app.settings.config import UVICORN_RELOAD
+from app.settings.config import UVICORN_RELOAD, UVICORN_HOST
 from app.database.utils import init_database_models
 
 if __name__ == "__main__":
     run_async(init_database_models())
     uvicorn.run(
         app="app.api.register_endpoints:app",
-        host="0.0.0.0",
+        host=UVICORN_HOST,
         port=8000,
         reload=UVICORN_RELOAD
     )
