00213ab393478258f2ea183f9dc70030953c8e91
Andrew <vesell4123@mail.ru>
2023-03-14 02:34:57 +0300

    remove unused dependency

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import uvicorn
-from tortoise import run_async
 
 from app.settings.config import UVICORN_RELOAD, UVICORN_HOST
-from app.database.utils import init_database_models
 
 if __name__ == "__main__":
-    run_async(init_database_models())
     uvicorn.run(
         app="app.api.register_endpoints:app",
         host=UVICORN_HOST,
