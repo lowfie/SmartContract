cffd48dadcf7c91661d189fcd65cdec68e00f0d7
Andrew <vesell4123@mail.ru>
2023-03-14 02:34:16 +0300

    register tortoise with fastapi

--- app/api/register_endpoints.py
+++ app/api/register_endpoints.py
@@ -1,13 +1,38 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.api.healthcheck.route import healthcheck
 from app.api.tokens.route import tokens
-from app.settings.config import API_NAME, API_VERSION
+from app.settings.config import (
+    USER_POSTGRES,
+    PASSWORD_POSTGRES,
+    HOST_POSTGRES,
+    PORT_POSTGRES,
+    DATABASE_POSTGRES,
+    API_NAME,
+    API_VERSION
+)
 
 app = FastAPI(
     title=API_NAME,
     version=API_VERSION
 )
+
+url = "asyncpg://{user}:{password}@{host}:{port}/{database}".format(
+        user=USER_POSTGRES,
+        password=PASSWORD_POSTGRES,
+        host=HOST_POSTGRES,
+        port=PORT_POSTGRES,
+        database=DATABASE_POSTGRES
+    )
+
+register_tortoise(
+        app,
+        db_url=url,
+        modules={"models": ["app.database.models"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
 
 app.include_router(healthcheck, tags=["healthcheck"])
 app.include_router(tokens, tags=["tokens"])
