35f056939961d3a83c7d1f7a6cdd9e0281bdaf36
Andrew <vesell4123@mail.ru>
2023-03-13 23:32:59 +0300

    create EthereumGoerliTestnet contract class

---
+++ app/web3/contract/ethereum_goerli.py
@@ -0,0 +1,19 @@
+from app.web3.base import w3
+
+
+class EthereumGoerliTestnet:
+    def __init__(self, contract_address: str, contract_abi: str) -> None:
+        self._contract_address = contract_address
+        self._contract_abi = contract_abi
+
+        self.contract_instance = w3.eth.contract(
+            address=self._contract_address,
+            abi=self._contract_abi
+        )
+
+    def total_supply(self) -> int | None:
+        try:
+            result = self.contract_instance.functions.totalSupply().call()
+        except Exception as _ex:
+            result = None
+        return result
