605a9efd899cee8a65f68873e9cc1e0e9d6d9d8c
Andrew <vesell4123@mail.ru>
2023-03-14 18:22:37 +0300

    change field of token model

--- app/database/models.py
+++ app/database/models.py
@@ -4,7 +4,7 @@ from tortoise.models import Model
 
 class Token(Model):
     id = fields.IntField(pk=True)
-    unique_hash = fields.CharField(max_length=256, unique=True, null=True)
-    tx_hash = fields.CharField(max_length=256, null=True)
+    unique_hash = fields.CharField(max_length=64, unique=True, null=True)
+    tx_hash = fields.TextField(null=True)
     media_url = fields.TextField(null=True)
     owner = fields.TextField(null=True)
